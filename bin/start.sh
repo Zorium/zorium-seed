c1b82411761b32fd37e059076fab3ff55a7e49c3
Zolmeister <zolikahan@gmail.com>
2015-06-03 14:49:38 -0700

    start.sh: REPLACE_ENV escape characters, fixes #20

--- bin/start.sh
+++ bin/start.sh
@@ -30,7 +30,7 @@ while read -d $'\0' -r file; do
   while read line; do
     if [[ $line =~ REPLACE_ENV_([A-Z0-9_]+) ]]; then
       env_name="${BASH_REMATCH[1]}"
-      env_value=$(eval "echo \$$env_name")
+      env_value=$(echo $(eval "echo \$$env_name") | sed -e 's/[\/&]/\\&/g')
       echo "replacing $env_name with '$env_value'" | tee $LOG_DIR/$LOG_NAME.build.log
       sed -i s/REPLACE_ENV_$env_name/\"$env_value\"/g $file
     fi
